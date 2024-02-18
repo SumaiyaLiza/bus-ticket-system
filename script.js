e555a88a22fff291a8e147f45c9acae543b29448
Sumaiya Liza <lizasumaiya81@gmail.com>
2024-02-19 01:04:51 +0600

    commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+// hide and go to the seat page 
+
 function Button() {
     const Button = document.getElementById('home-screen');
     Button.classList.add('hidden');
@@ -7,25 +9,55 @@ function Button() {
     Seat.classList.remove('hidden');
 }
 
+//  if clicked that will be green otherwise white and two click is white and one click is green 
 
 const btns = document.querySelectorAll('.a');
 
-btns.forEach(btn => {
+for (let i = 0; i < btns.length; i++) {
+  let clicked = false;
+  const btn = btns[i];
   btn.addEventListener('click', function onClick() {
-    btn.style.backgroundColor = '#1DD100';
-    btn.style.color = 'white';
+    clicked = !clicked;
+    if (clicked) {
+      btn.style.backgroundColor = '#1DD100';
+      btn.style.color = 'white';
+    } else {
+      btn.style.backgroundColor = '';
+      btn.style.color = '';
+    }
   });
-});
+}
 
-
-
+// seat number,available,selected 
 const seatButtons = document.querySelectorAll('.seat-btn');
+const availableCountElement = document.querySelector('.available-count');
+const selectedCountElement = document.querySelector('.selected-count');
+const seatCountElement = document.querySelector('.seat-count');
+
+let selectedSeats = 0;
+let availableSeats = 40; 
 
 seatButtons.forEach(btn => {
   btn.addEventListener('click', function onClick() {
-    // Update Seat count
-    const seatCountElement = document.querySelector('.seat-count');
-    seatCountElement.textContent = parseInt(seatCountElement.textContent) + 1;
+    if (!btn.classList.contains('selected')) {
+      btn.classList.add('selected');
+      selectedSeats++;
+      availableSeats--;
+    } else {
+      btn.classList.remove('selected');
+      selectedSeats--;
+      availableSeats++;
+    }
+    availableCountElement.textContent = availableSeats + " Available";
+    selectedCountElement.textContent = selectedSeats + " Selected";
+    seatCountElement.textContent = selectedSeats;
   });
 });
+
+
+
+
+
+
+
 
