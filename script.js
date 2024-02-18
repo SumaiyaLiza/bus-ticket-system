ea721cddc922a41d0b2d26ed7deb067e4022be14
Sumaiya Liza <lizasumaiya81@gmail.com>
2024-02-18 23:23:18 +0600

    commit

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ btns.forEach(btn => {
 });
 
 
+
+const seatButtons = document.querySelectorAll('.seat-btn');
+
+seatButtons.forEach(btn => {
+  btn.addEventListener('click', function onClick() {
+    // Update Seat count
+    const seatCountElement = document.querySelector('.seat-count');
+    seatCountElement.textContent = parseInt(seatCountElement.textContent) + 1;
+  });
+});
+
